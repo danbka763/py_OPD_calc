9d3e2ea0153b4af25f04047fe41d70897c6ec221
Having-no-guise <eskelanir@mail.ru>
2022-03-17 00:31:40 +0700

    main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+'''import keras as k
+import numpy as np
 
+input_data = np.array([0.3, 0.7, 0.9])
+output_data = np.array([0.5, 0.7, 1.0])
+
+model = k.Sequential()
+model.add(k.layers.Dense(units=1, activation="linear"))
+model.compile(loss="mse", optimizer="sgd")
+fit_results = model.fit(x=input_data, y = output_data, epochs= 100)
+'''
 
 height = 800
 width = 700
